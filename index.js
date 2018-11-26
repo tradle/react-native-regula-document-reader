a5384fd41d2d9f68c77df4cc8a6d180ec5ea7499
pgmemk <ellen@tradle.io>
2018-11-26 12:01:03 -0500

    feat: more scenarios added

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ const Scenario = {
   mrz: 'Mrz',
   ocr: 'Ocr',
   barcode: 'Barcode',
+  locate: 'Locate',
+  docType: 'DocType',
+  mrzOrBarcode: 'MrzOrBarcode',
+  mrzOrLocate: 'MrzOrLocate',
+  mrzAndLocate: 'MrzAndLocate',
+  mrzOrOcr: 'MrzOrOcr',
+  mrzOrBarcodeOrOcr: 'MrzOrBarcodeOrOcr',
+  locateVisual_And_MrzOrOcr: 'LocateVisual_And_MrzOrOcr',
+  fullProcess: 'FullProcess',
+  id3Rus: 'Id3Rus',
 }
 
 export default {
