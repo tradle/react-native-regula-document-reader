a749cba1bcf805ad448eceb8f3a50d4f7e83e15e
pgmemk <ellen@tradle.io>
2018-12-21 18:18:53 -0500

    refactor: dupplicate check for hintMessages

--- android/src/main/java/com/regula/documentreader/RegulaConfig.java
+++ android/src/main/java/com/regula/documentreader/RegulaConfig.java
@@ -56,10 +56,6 @@ public class RegulaConfig {
       customization.showHelpAnimation = opts.getBoolean("showHelpAnimation");
     }
 
-    if (opts.hasKey("showHintMessages")) {
-      customization.showHintMessages = opts.getBoolean("showHintMessages");
-    }
-
     if (opts.hasKey("cameraFrameDefaultColor")) {
       customization.cameraFrameDefaultColor = opts.getString("cameraFrameDefaultColor");
     }
